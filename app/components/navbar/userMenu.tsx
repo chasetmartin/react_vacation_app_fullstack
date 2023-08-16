46f0ac65fab7195da265ccc2192ab2d62963ce32
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-08-16 11:22:42 -0700

    Google and Github OAuth implementation

--- app/components/navbar/userMenu.tsx
+++ app/components/navbar/userMenu.tsx
@@ -6,11 +6,11 @@ import { useCallback, useState } from 'react';
 import MenuItem from './menuItem';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import useLoginModal from '@/app/hooks/useLoginModal';
-import { User } from '@prisma/client';
 import { signOut } from 'next-auth/react';
+import { safeUser } from '@/app/types';
 
 interface UserMenuProps {
-    currentUser?: User | null;
+    currentUser?: safeUser | null;
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({
@@ -64,7 +64,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                 >
                     <AiOutlineMenu />
                     <div className="hidden md:block">
-                        <Avatar />
+                        <Avatar src={currentUser?.image}/>
                     </div>
                 </div>
             </div>
