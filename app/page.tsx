695f875efac8792744cb87b2458bd378982a45ad
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-08-18 08:57:57 -0700

    Finished listing creation

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,35 @@
+import ClientOnly from "./components/clientOnly";
+import Container from "./components/container";
+import EmptyState from "./components/emptyState";
 
 export default function Home() {
+  const isEmpty = true;
+
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState />
+      </ClientOnly>
+    )
+  }
+  
   return (
-    <div className="text-rose-500 text-2xl">Hello Vacation Rentals!</div>
+    <ClientOnly>
+      <Container>
+        <div className="
+          pt-24
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        ">
+          <div>My future listings</div>
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
